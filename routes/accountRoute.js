8ab8524f9e51527efcc041cd9748601db05765a2
7pigy7 <7pigy7@gmail.com>
2025-05-27 15:15:29 -0700

    validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,11 +2,18 @@
 const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities/index")
+const regValidate = require('../utilities/account-validation')
 const accController = require("../controllers/accController")
 
 // Route to build inventory by classification view
 router.get("/login", utilities.handleErrors(accController.buildLogin));
 router.get("/register", utilities.handleErrors(accController.buildRegister));
-router.post('/register', utilities.handleErrors(accController.registerAccount));
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accController.registerAccount)
+)
 
 module.exports = router;
